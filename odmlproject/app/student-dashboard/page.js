3abc5f9997ac6c3fcc9ccf75692f31cbce642a98
ik04 <ishaankhurana123@gmail.com>
2023-09-23 01:43:11 +0530

    feat: created od cards

--- odmlproject/app/student-dashboard/page.js
+++ odmlproject/app/student-dashboard/page.js
@@ -1,3 +1,4 @@
+import OdCard from "@/components/student/odCards";
 import StudentNavbar from "@/components/student/studentNavbar";
 import React from "react";
 
@@ -5,6 +6,11 @@ const page = () => {
   return (
     <main className="flex flex-col w-full min-h-screen">
       <StudentNavbar />
+      <div className="">
+        <OdCard title={"Test"} status={0} />
+        <OdCard title={"Test"} status={1} />
+        <OdCard title={"Test"} status={2} />
+      </div>
     </main>
   );
 };
--- odmlproject/components/student/odCards.js
+++ odmlproject/components/student/odCards.js
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-const odCards = () => {
-  return <div></div>;
+const OdCard = ({ title, status, createdOn }) => {
+  let statusObj = {
+    text: "placeholder",
+    color: "#cccccc",
+  };
+
+  if (status === 0) {
+    statusObj = {
+      text: "Applied",
+      color: "success",
+    };
+  }
+  if (status === 1) {
+    statusObj = {
+      text: "Rejected",
+      color: "reject",
+    };
+  }
+  if (status === 2) {
+    statusObj = {
+      text: "Under Review",
+      color: "underReview",
+    };
+  }
+  console.log(statusObj);
+
+  return (
+    <div className="md:w-[700px] md:h-[120px] rounded-[10px] border border-[#DFCE4B] md:p-5 flex flex-col justify-center">
+      <h1 className="md:text-[40px] font-bold text-[#484848]">{title}</h1>
+      <h2 className="text-[20px]">
+        Status:{" "}
+        <span className={`text-${statusObj.color}`}>{statusObj.text}</span>
+      </h2>
+    </div>
+  );
 };
 
-export default odCards;
+export default OdCard;
