6bb16c2ec251c3312fd8143b50427d7a6c98f7f4
ik04 <ishaankhurana123@gmail.com>
2023-10-22 18:03:03 +0530

    feat: use prettier to format files

--- odmlproject/app/page.js
+++ odmlproject/app/page.js
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import Navbar from '@/components/navbar'
-import Hero from '@/components/hero'
+import Image from "next/image";
+import Navbar from "@/components/navbar";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full min-h-screen ">
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
     </main>
-  )
+  );
 }
